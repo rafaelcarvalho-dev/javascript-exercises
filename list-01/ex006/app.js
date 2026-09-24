7b90713d1f6f774474e45369cb8459e430ba889a
rafael carvalho <carva.rafael175@gmail.com>
2026-09-24 11:37:51 -0300

    feat: add exercise 006 from list-01

---
+++ list-01/ex006/app.js
@@ -0,0 +1,8 @@
+let mensagem_01 = "estou aprendendo JavaScript!";
+
+
+document.getElementById("resultado_01").innerHTML = mensagem_01
+document.getElementById("resultado_02").innerHTML = mensagem_01
+document.getElementById("resultado_03").innerHTML = mensagem_01
+document.getElementById("resultado_04").innerHTML = mensagem_01
+document.getElementById("resultado_05").innerHTML = mensagem_01
